2733be0285fe5738237bfe8125927aab18fe8512
Dhaval Solanki <dhaval.d.solanki@gmail.com>
2022-12-14 03:36:45 +0530

    added logo

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -3,7 +3,10 @@ function Header(): JSX.Element {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div className="container-fluid">
-				<a className="navbar-brand" href="#">VM Empire</a>
+				<a className="navbar-brand d-flex justify-content-center align-items-center" href="#">
+					<img src={process.env.PUBLIC_URL + '/logo.svg'} alt="Logo" className="navbar-logo" />
+					<span>VM Empire</span>
+				</a>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
