e4a23617b6c2bbd7b34f848d4ad099f7cbf5f091
Dhaval Solanki <dhaval.d.solanki@gmail.com>
2022-12-14 01:32:26 +0530

    no message

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 function Header(): JSX.Element {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div className="container-fluid">
 				<a className="navbar-brand" href="#">VM Empire</a>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
