4954a5e45e8e6493831b3d0935c4ca6c2166a941
Dhaval Solanki <dhaval.d.solanki@gmail.com>
2022-12-14 15:42:07 +0530

    no message

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/js/bootstrap.min.js";
-
+/* eslint-disable no-restricted-globals */
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
