79b47e361aefdb465fe142f91ff52ebec045432a
Dhaval Solanki <dhaval.d.solanki@gmail.com>
2022-12-14 15:51:23 +0530

    no message

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 function Header(): JSX.Element {
+	const [isRefreshing, setIsRefreshing] = useState(false);
 	const refresh = () => { 
-		window.location.reload();	
+		setIsRefreshing(true);
+		setTimeout(()=>{
+			window.location.reload();	
+			setIsRefreshing(false);
+		}, 2000)
 	};
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div className="container-fluid">
 				<a className="navbar-brand d-flex justify-content-center align-items-center" href="#">
 					<img src={process.env.PUBLIC_URL + '/logo.svg'} alt="Logo" className="navbar-logo" />
-					<span className='me-2' onClick={(e) => refresh()}>VM Empire</span>
-					<i className="bi bi-arrow-clockwise"></i>
-					{/* <button type="button" className="btn btn-secondary mt-3" onClick={(e) => refresh()}>
-					</button> */}
+					<div className='me-2'>VM Empire</div>
+					{ !isRefreshing && <i className="bi bi-arrow-clockwise" onClick={(e) => refresh()}></i>}
+					{isRefreshing && <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>}
 				</a>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
